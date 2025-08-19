eef6c0dc44aaa7abbbefc9194af77dbf52bf6d81
MuhammadSalmanAziz <m.salmanaziz12@gmail.com>
2025-08-19 18:49:53 +0500

    main file added

--- app/main.py
+++ app/main.py
@@ -1,13 +1,47 @@
 # Practing Fast API
 
-from fastapi import FastAPI
+from fastapi import FastAPI,Query
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class User(BaseModel):
+    name:str
+    password:str
+    address: Optional[str]= None
 
 @app.get("/")
 def index():
     return "Hello World"
 
-@app.get("/1")
-def index1():
-    return {"name": "salman"}
+# Practicing path parameters
+# how it looks like
+# https://salman.com/items/item_id(1,2,3 etc)
+
+# @app.get("/items/{item_id}")
+# def index_2(item_id:int):
+#     return {"product_id": item_id}
+
+# # Query Parameters
+
+# @app.get("/items/")
+# def que(q:int=0, m : Optional[int]= 10):
+#     return {"product": q ," m": m}
+
+# #filepath
+
+# @app.get("/items/{filepath:path}")
+# def ind(filepath:str):
+#     return {"filepath": filepath}
+
+
+@app.get("/items")
+def index(q: Optional[str]=Query(None,max_length=5,min_length=2)):
+    return {"q":q}
+
+@app.post("/items/{user_id}")
+def index(user_id:int,user:User):
+    print(user_id)
+    return user
+    return "hello world"
